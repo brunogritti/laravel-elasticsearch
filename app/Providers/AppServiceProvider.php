4f17a3bfb92e81ea639f45780023da01e2a0127d
Bruno Gritti <glbrunogl@gmail.com>
2023-06-08 21:32:19 -0300

    reindex command

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,42 @@
 
 namespace App\Providers;
 
+use App\Articles;
+use Elasticsearch\Client;
+use Elasticsearch\ClientBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
-        $this->app->bind(
-            \App\Repositories\ArticlesRepository::class,
-            \App\Repositories\EloquentRepository::class
-        );
+        $this->app->bind(Articles\ArticlesRepository::class, function ($app) {
+            // This is useful in case we want to turn-off our
+            // search cluster or when deploying the search
+            // to a live, running application at first.
+            if (! config('services.search.enabled')) {
+                return new Articles\EloquentRepository();
+            }
+
+            return new Articles\ElasticsearchRepository(
+                $app->make(Client::class)
+            );
+        });
+
+        $this->bindSearchClient();
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    private function bindSearchClient()
     {
-        //
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts($app['config']->get('services.search.hosts'))
+                ->build();
+        });
     }
 }
